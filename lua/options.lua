dd039bb09849853c1617d8af9fcd046194c59e57
jeroenwzelf <jeroen.donkers@live.nl>
2024-12-18 22:04:06 +0100

    Prefer cursor in middle of screen

--- lua/options.lua
+++ lua/options.lua
@@ -67,6 +67,6 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 999
 
 -- vim: ts=2 sts=2 sw=2 et
